5563d33a18e45f64f238a53f3c857b9bc9e19c90
luuzun <luuzun@naver.com>
2017-03-27 23:13:31 +0900

    Look And Feel 초기화 완료

--- src/main/java/kr/or/dgit/erp_luuzun/view/TestFrame.java
+++ src/main/java/kr/or/dgit/erp_luuzun/view/TestFrame.java
@@ -1,13 +1,18 @@
 package kr.or.dgit.erp_luuzun.view;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.text.ParseException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
-import de.javasoft.plaf.synthetica.*;
 
+import de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel;
+import de.javasoft.plaf.synthetica.SyntheticaLookAndFeel;
+import de.javasoft.plaf.synthetica.SyntheticaStandardLookAndFeel;
 import kr.or.dgit.erp_luuzun.content.viewSale;
 public class TestFrame extends JFrame {
 
@@ -20,14 +25,20 @@ public class TestFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+
 		try {
-			UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel");
-		} catch(Exception e) {
+			UIManager.setLookAndFeel(new SyntheticaStandardLookAndFeel());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		try {
+			UIManager.setLookAndFeel(new SyntheticaAluOxideLookAndFeel());
+			SyntheticaLookAndFeel.setFont("인터파크고딕 L", 12);
+		} catch (UnsupportedLookAndFeelException | ParseException e) {
 			e.printStackTrace();
 		}
 		
-		viewSale viewSale = new viewSale();		
+		viewSale viewSale = new viewSale();
 		add(viewSale);
 	}
-
 }
